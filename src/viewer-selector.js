719cc09f23e949f11b42ea9f7b58d202d3f309f5
North McCormick <northmccormick@icloud.com>
2019-11-08 11:50:33 -0800

    [Cardboard Updates] feat: dark mode baby 22

--- src/viewer-selector.js
+++ src/viewer-selector.js
@@ -125,7 +125,7 @@ ViewerSelector.prototype.createDialog_ = function(options) {
   var width = 280;
   var dialog = document.createElement('div');
   var s = dialog.style;
-  s.background = '#000000';
+  s.backgroundColor = '#000000';
   s.color = '#FFFFFF';
   s.boxSizing = 'border-box';
   s.position = 'fixed';
@@ -135,7 +135,6 @@ ViewerSelector.prototype.createDialog_ = function(options) {
   s.width = width + 'px';
   s.padding = '24px';
   s.overflow = 'hidden';
-  s.background = '#fafafa';
   s.fontFamily = "'Roboto', sans-serif";
   s.boxShadow = '0px 5px 20px #666';
 
